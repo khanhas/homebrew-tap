56e490bd5f9a7bf60140ec48c9d71db7abb4b28d
khanhas <xuankhanh963@gmail.com>
2019-03-05 21:36:15 +1000

    bump: v0.5.1

--- spicetify-cli.rb
+++ spicetify-cli.rb
@@ -1,33 +1,21 @@
-require "language/go"
 class SpicetifyCli < Formula
   desc "Command-line tool to customize Spotify client"
   homepage "https://github.com/khanhas/spicetify-cli"
-  url "https://github.com/khanhas/spicetify-cli/archive/v0.4.1.tar.gz"
-  sha256 "29b065c3b5b1acab71977371fcb488b2f9af4d854ce1e57f9522398799894a7d"
+  url "https://github.com/khanhas/spicetify-cli/archive/v0.5.1.tar.gz"
+  sha256 "024f71488286da3588eb6543104138ad4ddc8248d111c634069d24a848163913"
   depends_on "go" => :build
-
-  go_resource "github.com/go-ini/ini" do
-    url "https://github.com/go-ini/ini.git"
-  end
-
-  go_resource "gopkg.in/cheggaaa/pb.v2" do
-    url "https://gopkg.in/cheggaaa/pb.v2"
-  end
-
-  go_resource "gopkg.in/mattn/go-colorable.v0" do
-    url "https://gopkg.in/mattn/go-colorable.v0"
-  end
 
   def install
     ENV["GOPATH"] = buildpath/"dep"
     buildpath.install
-    Language::Go.stage_deps resources, buildpath/"dep"
 
     cd buildpath do
       system "go", "build", "-o", "spicetify"
       bin.install "spicetify"
-      system "cp", "-r", "./jsHelper", "./Themes", "./Extensions", "./globals.d.ts", bin
-      prefix.install_metafiles
+      cp_r "./globals.d.ts", bin
+      cp_r "./jsHelper", bin
+      cp_r "./Themes", bin
+      cp_r "./Extensions", bin
     end
   end
 
