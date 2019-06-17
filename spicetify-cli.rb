449c34027884ec294ed0612aed83bddad69384d3
khanhas <xuankhanh963@gmail.com>
2019-06-17 19:23:53 +1000

    bump: v0.9.2

--- spicetify-cli.rb
+++ spicetify-cli.rb
@@ -1,8 +1,8 @@
 class SpicetifyCli < Formula
   desc "Command-line tool to customize Spotify client"
   homepage "https://github.com/khanhas/spicetify-cli"
-  url "https://github.com/khanhas/spicetify-cli/archive/v0.9.1.tar.gz"
-  sha256 "3b1a2a95d2b4a7144a3304cc943f6d60adada2f0cf3b3e37daecbc2c3b1ce6a1"
+  url "https://github.com/khanhas/spicetify-cli/archive/v0.9.2.tar.gz"
+  sha256 "8c214568c1211420b8b999de97eb1eafc7b3eca9fa58b29a54dd5ddfb99cbecc"
   depends_on "go" => :build
 
   def install
