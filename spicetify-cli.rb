6fa2889c05d94521b58881c25762e28a0cc964a3
itsmeow <itsmeow@itsmeow.dev>
2021-11-04 13:47:18 +0100

    Update to 2.7.2

--- spicetify-cli.rb
+++ spicetify-cli.rb
@@ -1,8 +1,8 @@
 class SpicetifyCli < Formula
   desc "Command-line tool to customize Spotify client"
   homepage "https://github.com/khanhas/spicetify-cli"
-  url "https://github.com/khanhas/spicetify-cli/archive/v2.7.1.tar.gz"
-  sha256 "477a2d70485b08e3d35db5b1e7fc82b3c5b675d9ed7e065bac9d383c7495be69"
+  url "https://github.com/khanhas/spicetify-cli/archive/v2.7.2.tar.gz"
+  sha256 "36abc3bb39d04794aafe58e95e31156c3375707f9bc068c2b5551f89066ff28a"
   depends_on "go" => :build
 
   def install
