7d8430eab577d74742d2863fc2d5ba7675dd87bb
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-11-12 16:51:33 -0500

    spicetify-cli 2.27.0

--- spicetify-cli.rb
+++ spicetify-cli.rb
@@ -1,26 +1,26 @@
-require "language/go"
-
 class SpicetifyCli < Formula
   desc "Command-line tool to customize Spotify client"
   homepage "https://github.com/spicetify/spicetify-cli"
-  url "https://github.com/spicetify/spicetify-cli/archive/v2.24.2.tar.gz"
-  head "https://github.com/spicetify/spicetify-cli"
-  sha256 "7c9b68289e5fe0eef945138a520496f276e03e72395a22b9ab52b5d3ae83ebb8"
+  url "https://github.com/spicetify/spicetify-cli/archive/refs/tags/v2.27.0/v2.27.0.tar.gz"
+  sha256 "824ac8341f5833277a7177a7e17af7036a173e01acbc0730df619c035e0de16b"
+  head "https://github.com/spicetify/spicetify-cli.git", branch: "master"
 
-  depends_on "go" => "1.18"
+  depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     path = buildpath/"dep"
     path.install Dir["*"]
     cd path do
-      system "go", "build", "-ldflags", "-X main.version=#{version.to_s}", "-o", "#{bin}/spicetify"
-      cp_r "./globals.d.ts", bin
-      cp_r "./jsHelper", bin
-      cp_r "./Themes", bin
-      cp_r "./Extensions", bin
-      cp_r "./CustomApps", bin
-      cp_r "./css-map.json", bin
+      system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}"), "-o", "#{bin}/spicetify"
+      bin.install [
+        "css-map.json",
+        "CustomApps",
+        "Extensions",
+        "globals.d.ts",
+        "jsHelper",
+        "Themes",
+      ]
     end
   end
 
